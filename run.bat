7b542b78145e3f64fdff114d36ae0eaabc26a704
Ian Scott <ian.scott@thomsonreuters.com>
2024-02-17 18:14:08 +0000

    Make run.bat actually work on windoze

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-java -cp target/lib/sqlite-jdbc-3.39.2.1.jar:target/sqlitetest-0.0.1.jar com.sdfeu.yane.sqllitetest.SQLiteTest %1 %2 %3
+java -cp target\lib\sqlite-jdbc-3.39.2.1.jar;target\sqlitetest-0.0.1.jar com.sdfeu.yane.sqllitetest.SQLiteTest %1 %2 %3
